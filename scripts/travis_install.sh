5cb11362c0570f9f4c26d1c4d1b9c03dfc71f95f
Andreas Fertig <mail@cppinsights.io>
2018-05-26 18:50:30 +0200

    Enable travis-ci caching.
    Add caching capability with ccache and for brew packages. Reduce the
    noise of console output and skip brew auto update.

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,16 +1,19 @@
 #! /bin/sh
 
 if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+   export HOMEBREW_NO_AUTO_UPDATE=1
    brew install cmake || brew upgrade cmake
    brew install xz || brew upgrade xz
 
+   brew install ccache
+   export PATH="/usr/local/opt/ccache/libexec:$PATH"
+
    mkdir ${TRAVIS_BUILD_DIR}/clang
    cd ${TRAVIS_BUILD_DIR}/clang
-   wget https://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
+   wget -q --continue --directory-prefix=$HOME/Library/Caches/Homebrew https://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
+   cp $HOME/Library/Caches/Homebrew/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
 
-   xz -vd clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
+   xz -d clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
    tar -xf clang+llvm-6.0.0-x86_64-apple-darwin.tar
    mv clang+llvm-6.0.0-x86_64-apple-darwin current
-
-   ls -l ${TRAVIS_BUILD_DIR}/clang/current/bin/clang
 fi
