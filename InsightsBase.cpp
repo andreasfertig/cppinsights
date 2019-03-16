e961037600a4704bc054c13fccef87689e0f3418
Andreas Fertig <mail@cppinsights.io>
2019-03-16 11:20:36 +0100

    Removed unused code.

--- InsightsBase.cpp
+++ InsightsBase.cpp
@@ -11,20 +11,6 @@
 
 namespace clang::insights {
 
-bool InsightsBase::SkipIfAlreadySeen(const Stmt* stmt)
-{
-    const intptr_t addr{reinterpret_cast<intptr_t>(stmt)};
-    if(mMap.find(addr) != mMap.end()) {
-        DPrint("duplicate\n");
-        return true;
-    }
-
-    mMap[addr] = true;
-
-    return false;
-}
-//-----------------------------------------------------------------------------
-
 void InsightsBase::InsertIndentedText(SourceLocation loc, OutputFormatHelper& outputFormatHelper)
 {
     mRewrite.InsertText(loc, outputFormatHelper.GetString(), true, true);
