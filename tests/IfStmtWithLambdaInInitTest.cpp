f3956ded1fbcf50a12224dacdaf38bbc9feb7bbf
Andreas Fertig <mail@cppinsights.io>
2019-03-23 10:15:15 +0100

    Added more tests.
    Added implicit cast test in template and IfStmt with lambda in its init
    test.

---
+++ tests/IfStmtWithLambdaInInitTest.cpp
@@ -0,0 +1,6 @@
+int main()
+{
+    if(int x=[]{ return 2;}(), y=[]{ return 3;}(),z=[]{ return 4;}(); true)
+    {
+    }
+}
---
+++ tests/IfStmtWithMultipleVarDeclsTest.cpp
@@ -0,0 +1,6 @@
+int main()
+{
+    if(int x=1, y=2,z=3; true)
+    {
+    }
+}
