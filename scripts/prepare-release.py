0b7637fcf4e28b628a272f13ffe59187e361a62d
Andreas Fertig <andy@cppinsights.io>
2022-02-03 18:39:01 +0100

    Updated release script

--- scripts/prepare-release.py
+++ scripts/prepare-release.py
@@ -12,29 +12,25 @@ import subprocess
 #------------------------------------------------------------------------------
 
 def main():
-    oldClangDevel  = '9'
-    newClangDevel = '13'
-    oldClangStable = '10'
-    newClangStable = '12'
-    oldInsightsVersion = '0.6'
-    newInsightsVersion = '0.7'
+    versionH = open('version.h.in', 'r').read()
+
+    oldClangStable = '12'
+    newClangStable = '13'
+    newInsightsVersion = '0.8'
+    oldInsightsVersion = re.search('INSIGHTS_VERSION\s+"(.*?)"', versionH, re.DOTALL | re.MULTILINE).group(1)
+
 
     print('Preparing a new release:')
-    print(' Current Clang development : %s' %(oldClangDevel))
-    print(' New Clang development     : %s' %(newClangDevel))
     print(' Current Clang stable      : %s' %(oldClangStable))
     print(' New Clang stable          : %s' %(newClangStable))
     print(' Current Insights version  : %s' %(oldInsightsVersion))
     print(' New Insights version      : %s' %(newInsightsVersion))
 
-
     print('  - Updating .github/workflows/ci.yml')
     travis = open('.github/workflows/ci.yml', 'r').read()
 
     regEx = re.compile('[clang|llvm]-([0-9]+)')
-#    regEx = re.compile('llvm-([0-9]+)', re.MULTILINE)
 
-    travis = re.sub('(clang|llvm|clang\+\+|llvm-config|llvm-toolchain-bionic|clang-format|clang-tidy|llvm-toolchain-trusty)(-%s)' %(oldClangDevel), '\\1-%s' %(newClangDevel) , travis)
     travis = re.sub('(clang|llvm|clang\+\+|llvm-config|llvm-toolchain-bionic|clang-format|clang-tidy|llvm-toolchain-trusty)(-%s)' %(oldClangStable), '\\1-%s' %(newClangStable) , travis)
     travis = re.sub('(clang|Clang|llvm|LLVM) (%s)' %(oldClangStable), '\\1 %s' %(newClangStable) , travis)
     travis = re.sub(r"(LLVM_VERSION=)('%s)" %(oldClangStable), r"\1'%s" %(newClangStable) , travis)
@@ -43,9 +39,6 @@ def main():
     travis = re.sub(r"clang(%s)0" %(oldClangStable), r"clang%s0" %(newClangStable) , travis)
     travis = re.sub(r"(llvm-toolchain-xenial)-(%s)" %(oldClangStable), r"\1-%s" %(newClangStable) , travis)
     travis = re.sub(r"(./llvm.sh) (%s)" %(oldClangStable), r"\1 %s" %(newClangStable) , travis)
-#    travis = re.sub('([clang|llvm])(-[0-9]+)', r'\1-%s' %(newClangDevel) , travis)
-#    print regEx.sub(travis, r'\1', newClangDevel)
-#    m = regEx.findall(travis)
 
 #    print(travis)
     open('.github/workflows/ci.yml', 'w').write(travis)
@@ -64,21 +57,33 @@ def main():
 
 
     print('  - Generating CHANGELOG.md')
-#    os.system('gren changelog --override --username=andreasfertig --repo=cppinsights %s...continous' %(oldInsightsVersion))
+    os.system('gren changelog --override --username=andreasfertig --repo=cppinsights %s...continous' %(oldInsightsVersion))
 
     cmd = ['gren', 'changelog', '--override', '--username=andreasfertig', '--repo=cppinsights', '%s...continous' %(oldInsightsVersion)]
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
 
     if 0 != p.returncode:
-        print('FAILED')
+        print('ERR: gren failed')
+        print(stderr)
         return 1
 
     changeLog = open('CHANGELOG.md', 'r').read()
     changeLog = re.sub('(## Continuous build.*?---)\n', '', changeLog, flags=re.DOTALL)
     open('CHANGELOG.md', 'w').write(changeLog)
 
-    print('  - Tagging v_%s' %(newInsightsVersion))
+    gitTag = 'v_%s' %(oldInsightsVersion)
+    print('  - Tagging %s' %(gitTag))
+
+    #cmd = ['git', 'tag', '-a', '-m', gitTag, gitTag, 'main']
+    cmd = ['git', 'tag', gitTag, 'main']
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+
+    if 0 != p.returncode:
+        print('ERR: git tag failed!')
+        print(stderr)
+        return 1
 
     cppInsightsDockerBaseFile = '../cppinsights-docker-base/Dockerfile'
 
