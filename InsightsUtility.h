e9537fc34ac30db8c53b5aa2c68ed07c3d68c01b
Andreas Fertig <andy@cppinsights.io>
2022-02-05 10:10:22 +0100

    Fixed `if` or `switch` with initialization.
    In the case where the initialization of an `if` or `switch` was only a
    call, the newline was missing.

--- InsightsUtility.h
+++ InsightsUtility.h
@@ -19,4 +19,7 @@
 using void_func_ref = llvm::function_ref<void()>;
 //-----------------------------------------------------------------------------
 
+template<typename T, typename... Ts>
+concept same_as_any_of = (std::same_as<T, Ts> || ...);
+
 #endif /* INSIGHTS_UTILITY_H */
